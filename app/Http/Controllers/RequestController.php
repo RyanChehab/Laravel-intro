ae4cfcaa5410972ae962d737a2f6870dee97012d
RyanChehab <ryanchehab20@gmail.com>
2024-11-24 13:44:18 +0200

    php <=>added store function in request controller

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class RequestController extends Controller
 {
-    //
+    public function store(HttpRequest $request)
+    {
+        // Validate the input
+        $validated = $request->validate([
+            'text' => 'required|string', 
+        ]);
+
+        
+        $newsRequest = \App\Models\Request::create([
+            'user_id' => auth()->id(),
+            'text' => $validated['text'],
+        ]);
+
+        // Return a response
+        return response()->json([
+            'success' => true,
+            'message' => 'News request submitted successfully!',
+            'data' => $newsRequest,
+        ]);
+    }
 }
