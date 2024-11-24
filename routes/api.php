b1454d19113fe5edc4867afad5e92c5dd20609ba
RyanChehab <ryanchehab20@gmail.com>
2024-11-24 12:54:41 +0200

    php <=> created routes

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,6 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('news',[NewsController::class, 'index']);
+Route::post('/news', [NewsController::class, 'store']);
+Route::put('/news/{id}', [NewsController::class, 'update']);
+Route::post('/news/{id}/restrictions', [NewsController::class, 'restrictions']);
