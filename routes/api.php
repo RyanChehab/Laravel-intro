889267a980b0681faba54c62dc87dd309b56a92e
RyanChehab <ryanchehab20@gmail.com>
2024-11-24 12:47:03 +0200

    php <=>added routes

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+//  App\Http\Controller\NewsController;
+use app\Http\Controllers\NewsController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('news',[NewsController::class, 'index']);
